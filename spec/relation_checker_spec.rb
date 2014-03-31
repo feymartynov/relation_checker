0c09dcbfbdc8150b86bcfd33d40ebf3a5de527d1
Timofey Martynov <feymartynov@gmail.com>
2014-03-31 21:52:07 +0200

    implement relation checker spec

--- spec/relation_checker_spec.rb
+++ spec/relation_checker_spec.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
 
 describe ActiveRecord::Base do
+  let(:checker) { double(:checker) }
+
+  before do
+    class DummyModel < ActiveRecord::Base; end
+    allow(RelationChecking::Checker).to receive(:new).with(DummyModel).and_return(checker)
+  end
+
   describe '.relation_checker' do
     it 'should return a new checker instance passing the object to it' do
-      pending
+      expect(DummyModel.relation_checker).to eq(checker)
     end
   end
 end
