ab3bf4e434bf7c1b25ef6632287118ebc9c8f7f1
Timofey Martynov <feymartynov@gmail.com>
2014-04-01 00:20:38 +0200

    implement values extractor spec

--- spec/clauses/where_clause/values_extractor_spec.rb
+++ spec/clauses/where_clause/values_extractor_spec.rb
@@ -1,9 +1,23 @@
 require 'spec_helper'
 
 describe RelationChecking::Checker::WhereClause::ValuesExtractor do
+  let(:values) { [double(:value, left: :some_key, right: :some_value),
+                  double(:value, left: :another_key, right: :another_value)] }
+  subject { RelationChecking::Checker::WhereClause::ValuesExtractor.new(values) }
+
+  before do
+    adapter = RelationChecking::Checker::WhereClause::ValuesExtractor::ValueAdapter
+
+    values.each do |value|
+      adapter_double = double(:value_adapter, left_value: value.left, right_value: value.right)
+      allow(adapter).to receive(:new).with(value).and_return(adapter_double)
+    end
+  end
+
   describe '#to_h' do
     it 'should return a hash containing left parts as keys and right parts as values' do
-      pending
+      values_hash = Hash[values.map { |v| [v.left, v.right] }]
+      expect(subject.to_h).to eq(values_hash)
     end
   end
 end
