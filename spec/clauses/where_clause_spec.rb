c529807367e48e265d9d082a1cd4dba45c2de20e
Timofey Martynov <feymartynov@gmail.com>
2014-04-01 00:08:58 +0200

    implement where clause spec

--- spec/clauses/where_clause_spec.rb
+++ spec/clauses/where_clause_spec.rb
@@ -1,13 +1,48 @@
 require 'spec_helper'
 
 describe RelationChecking::Checker::WhereClause do
+  let(:conditions) { {'public' => true, 'price' => 10..20} }
+  let(:relation) { double(:relation, where_values: conditions) }
+  subject { RelationChecking::Checker::WhereClause.new(conditions) }
+
   describe '#check_against' do
-    it 'should return true if all conditions are present in the given relation' do
-      pending
+    context do
+      before do
+        extractor = RelationChecking::Checker::WhereClause::ValuesExtractor
+        allow(extractor).to receive(:new).with(conditions)
+                            .and_return(double(:extractor, to_h: conditions))
+      end
+
+      it 'should return true if all conditions of the clause are present and match conditions in the relation' do
+        expect(subject.check_against(relation)).to eq(true)
+      end
+
+      it 'should be indifferent to hash keys' do
+        subject { RelationChecking::Checker::WhereClause.new(conditions.symbolize_keys) }
+        expect(subject.check_against(relation)).to eq(true)
+      end
     end
 
     it 'should return false if not all of the conditions are present in the given relation' do
-      pending
+      insufficient_conditions = conditions.dup.tap { |c| c.delete(c.keys.sample) }
+      relation = double(:relation, where_values: insufficient_conditions)
+
+      extractor = RelationChecking::Checker::WhereClause::ValuesExtractor
+      allow(extractor).to receive(:new).with(insufficient_conditions)
+                                       .and_return(double(:extractor, to_h: insufficient_conditions))
+
+      expect(subject.check_against(relation)).to eq(false)
+    end
+
+    it 'should return false if any pair of the condition values doesn\'t match' do
+      wrong_conditions = conditions.dup.tap { |c| c[c.keys.sample] = :wrong_value }
+      relation = double(:relation, where_values: wrong_conditions)
+
+      extractor = RelationChecking::Checker::WhereClause::ValuesExtractor
+      allow(extractor).to receive(:new).with(wrong_conditions)
+                                       .and_return(double(:extractor, to_h: wrong_conditions))
+
+      expect(subject.check_against(relation)).to eq(false)
     end
   end
 end
