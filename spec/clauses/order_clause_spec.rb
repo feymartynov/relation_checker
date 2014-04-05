2ca932f10f0533f22a330289416cc3fdb9ada00f
Timofey Martynov <feymartynov@gmail.com>
2014-04-05 06:02:39 +0400

    implement order clause spec

--- spec/clauses/order_clause_spec.rb
+++ spec/clauses/order_clause_spec.rb
@@ -1,21 +1,47 @@
 require 'spec_helper'
 
 describe RelationChecking::Checker::OrderClause do
+  let(:order_values) { ActiveSupport::OrderedHash[:created_at, :desc, :updated_at, :asc] }
+  let(:relation) { double(:relation,
+                          klass: double(:model_class, table_name: 'dummy_table'),
+                          arel: double(:arel, orders: relation_order_values)) }
+
+  subject { RelationChecking::Checker::OrderClause.new(order_values) }
+
   describe '#check_against' do
-    it 'should return true if all of the order keys are present in the given relation' do
-      pending
+    context 'relation\'s order values match expectations' do
+      let(:relation_order_values) { ['"dummy_table".created_at DESC',
+                                     '"dummy_table".updated_at ASC'] }
+
+      it 'should return true' do
+        expect(subject.check_against(relation)).to eq(true)
+      end
     end
 
-    it 'should return false if not all of the order keys are present in the given relation' do
-      pending
+    context 'not all of the order keys are present in the given relation' do
+      let(:relation_order_values) { ['"dummy_table".created_at DESC'] }
+
+      it 'should return false' do
+        expect(subject.check_against(relation)).to eq(false)
+      end
     end
 
-    it 'should return false if all of the order keys are present but not in the same order' do
-      pending
+    context 'all of the order keys are present but not in the same order' do
+      let(:relation_order_values) { ['"dummy_table".updated_at ASC',
+                                     '"dummy_table".created_at DESC'] }
+
+      it 'should return false' do
+        expect(subject.check_against(relation)).to eq(false)
+      end
     end
 
-    it 'should return false if any of the order directions doesn\'t match the expected one' do
-      pending
+    context 'some of the order directions don\'t match the expected one' do
+      let(:relation_order_values) { ['"dummy_table".created_at DESC',
+                                     '"dummy_table".updated_at DESC'] }
+
+      it 'should return false' do
+        expect(subject.check_against(relation)).to eq(false)
+      end
     end
   end
 end
